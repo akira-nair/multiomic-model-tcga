428d9329e3d01f69faab3688a4812176d266d5a0
akira-nair <akira_nair@brown.edu>
2023-02-13 00:46:11 -0500

    adding al modalities for ae v vae analysis

---
+++ models/MAVI/MAVI_VAE.py
@@ -0,0 +1,91 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+class Sampling(layers.Layer):
+    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+
+class VAE(keras.Model):
+    def __init__(self, input_dim, hidden_dim=512, latent_dim=32, lr = 0.0001, **kwargs):
+
+        # encoder
+        encoder_inputs = keras.Input(shape = (input_dim,), name = 'encoder_input')
+        
+        x = layers.Dense(hidden_dim, activation = 'relu', name = 'hidden_layer_1')(encoder_inputs)
+
+        z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+        z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+        z = Sampling()([z_mean, z_log_var])
+        encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+        encoder.summary()
+        # decoder
+        latent_inputs = keras.Input(shape=(latent_dim,), name = 'decoder_input')
+        x = layers.Dense(512, activation = 'relu', name = 'hidden_layer_2')(latent_inputs)
+        decoder_outputs = layers.Dense(input_dim, activation = None, name = 'reconstructed_input')(x)
+        decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+        decoder.summary()
+        super().__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = keras.losses.MeanSquaredError(data, reconstruction)
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+    @tf.function
+    def sample(self, eps=None):
+        if eps is None:
+        eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
+
+    def encode(self, x):
+        mean, logvar = tf.split(self.encoder(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.decoder(z)
+        if apply_sigmoid:
+        probs = tf.sigmoid(logits)
+        return probs
+        return logits
--- models/MAVI/ae_versus_vae.py
+++ models/MAVI/ae_versus_vae.py
@@ -9,84 +9,129 @@ Description :   This script is an analysis of how VAEs compare AEs for feature r
 import MAVI
 import unimodal_model as um
 import pandas as pd
-from sklearn.preprocessing import normalize
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import os
 import datetime
+import seaborn as sns
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
+sns.set()
 
 # FIXED HYPERPARAMETERS
-EPOCHS_UNSUPERVISED = 200
+EPOCHS_UNSUPERVISED = 150
 HIDDEN_DIM = 512
-LATENT_DIMS = [8, 32, 128, 256, 512]
+LATENT_DIMS = [32, 256]
 LEARNING_RATE_UNSUPERVISED = 0.001
 EPOCHS_SUPERVISED = 100
-LEARNING_RATE_SUPERVISED = 0.00001
+LEARNING_RATE_SUPERVISED = 0.001
 # Modalities
 CLINICAL = "/users/anair27/data/TCGA_Data/project_LUAD/data_processed/PRCSD_clinical_data.csv"
 CNV = "/users/anair27/data/TCGA_Data/project_LUAD/data_processed/PRCSD_cnv_data.csv"
 EPIGENOMIC = "/users/anair27/data/TCGA_Data/project_LUAD/data_processed/PRCSD_epigenomic_data.csv"
 TRANSCRIPTOMIC = "/users/anair27/data/TCGA_Data/project_LUAD/data_processed/PRCSD_transcriptomic_data.csv"
-MODALITY_TESTING = 'TRANSCRIPTOMIC'
+# MODALITY_TESTING = 'TRANSCRIPTOMIC'
 # Make output plot directories 
-OUTPUT_PLOTS = os.path.join('/users/anair27/data/anair27/singh-lab-TCGA-project/multiomic-model-tcga/__plots/', datetime.datetime.now())
+timestamp = datetime.datetime.now().strftime("%h-%d-%H:%M:%S")
+OUTPUT_PLOTS = os.path.join('/users/anair27/data/anair27/singh-lab-TCGA-project/multiomic-model-tcga/__plots/', timestamp)
 os.mkdir(OUTPUT_PLOTS)
+os.mkdir(os.path.join(OUTPUT_PLOTS, "cnv"))
+os.mkdir(os.path.join(OUTPUT_PLOTS, "epigenomic"))
+os.mkdir(os.path.join(OUTPUT_PLOTS, "transcriptomic"))
+norm = preprocessing.MinMaxScaler()
+ros = RandomOverSampler(random_state=42)
 
-if 'TRANSCRIPTOMIC'  == MODALITY_TESTING:
-    # for RNA-seq data
-    clinical_df = pd.read_csv(CLINICAL)
-    transcriptomic_df = pd.read_csv(TRANSCRIPTOMIC)
-    diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
-    data = transcriptomic_df.merge(diagnosis, on = "case_id").drop_duplicates()
-    train_df, test_df = train_test_split(data.iloc[:,2:].dropna(), test_size=0.1, random_state=42)
+def ae_versus_vae(modality: str, x_train, x_test, y_train, y_test): 
+    x_train = norm.fit_transform(x_train)
+    x_test = norm.fit_transform(x_test)
+    # x_train = norm.fit_transform(train_df.drop('vital_status_Dead', axis = 1))
+    # x_test = norm.fit_transform(test_df.drop('vital_status_Dead', axis = 1))
+    # y_train = train_df['vital_status_Dead']
+    # y_test = test_df['vital_status_Dead']
+    for LATENT_DIM in LATENT_DIMS:
+        ## vae
+        tf.keras.backend.clear_session()
+        vae = MAVI.VAE(input_dim = x_train.shape[1], 
+            hidden_dim=HIDDEN_DIM,
+            latent_dim=LATENT_DIM,   
+            activation = 'sigmoid',
+            lr = LEARNING_RATE_UNSUPERVISED)
+        vae.train(x_train, n_epochs= EPOCHS_UNSUPERVISED)
+        opt_curves = pd.DataFrame({'training_loss':vae.history.history['loss'], 'val_loss':vae.history.history['val_loss']})
+        opts = sns.lineplot(opt_curves)
+        opts.get_figure().savefig(os.path.join(OUTPUT_PLOTS, modality, f'VAE_OPTIMIZATION_{LATENT_DIM}dim.png'))
+        plt.clf()
+        vae.plot_latent_space(x_train, y_train, output=os.path.join(OUTPUT_PLOTS, modality, f'VAE_UMAP_{LATENT_DIM}dim.png'))
+        plt.clf()
+        x_train_vae_embed = vae.get_embedding(x_train)
+        x_test_vae_embed = vae.get_embedding(x_test)
+        # unimodal model
+        tf.keras.backend.clear_session()
+        model_on_vae, _ = um.create_model(x_train = x_train_vae_embed, y_train = y_train, n_epochs = EPOCHS_SUPERVISED, batch_size = None, lr = LEARNING_RATE_SUPERVISED, n_hidden = 1, dim_hidden = LATENT_DIM / 2)
+        opt_curves = pd.DataFrame({'training_loss':model_on_vae.history.history['loss'], 'val_loss':model_on_vae.history.history['val_loss']})
+        opts = sns.lineplot(opt_curves)
+        opts.get_figure().savefig(os.path.join(OUTPUT_PLOTS, modality, f'MODEL_ON_VAE_OPTIMIZATION_{LATENT_DIM}dim.png'))
+        plt.clf()
+        um.plot_confusion(model_on_vae, x_test_vae_embed, y_test, filepath = os.path.join(OUTPUT_PLOTS, modality, f'CM_FULL_MODEL_VAE_{LATENT_DIM}dim.png'))
+        plt.clf()
+        ## ae
+        tf.keras.backend.clear_session()
+        ae = um.create_ae(x_train = x_train, latent_dim = LATENT_DIM, epochs= EPOCHS_UNSUPERVISED, lr = LEARNING_RATE_UNSUPERVISED)
+        opt_curves = pd.DataFrame({'training_loss':ae.history.history['loss']})
+        opts = sns.lineplot(opt_curves)
+        opts.get_figure().savefig(os.path.join(OUTPUT_PLOTS, modality, f'AE_OPTIMIZATION_{LATENT_DIM}dim.png'))
+        plt.clf()
+        x_train_ae_embed = um.encode_input(ae, x_train)
+        x_test_ae_embed = um.encode_input(ae, x_test)
+        tf.keras.backend.clear_session()
+        model_on_ae, _ = um.create_model(x_train = x_train_ae_embed, y_train = y_train, n_epochs = EPOCHS_SUPERVISED, batch_size = None, lr = LEARNING_RATE_SUPERVISED, n_hidden = 1, dim_hidden = LATENT_DIM / 2)
+        opt_curves = pd.DataFrame({'training_loss':model_on_ae.history.history['loss'], 'val_loss':model_on_ae.history.history['val_loss']})
+        opts = sns.lineplot(opt_curves)
+        opts.get_figure().savefig(os.path.join(OUTPUT_PLOTS, modality, f'MODEL_ON_AE_OPTIMIZATION_{LATENT_DIM}dim.png'))
+        plt.clf()
+        um.plot_confusion(model_on_ae, x_test_ae_embed, y_test, filepath = os.path.join(OUTPUT_PLOTS, modality, f'CM_FULL_MODEL_AE_{LATENT_DIM}dim.png'))
+        plt.clf()
 
-# TODO: correct the train test split for these modalities
-elif 'CNV'  == MODALITY_TESTING:
-    # for CNV data
-    clinical_df = pd.read_csv(CLINICAL)
-    cnv_df = pd.read_csv(CNV)
-    diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
-    data = cnv_df.merge(diagnosis, on = "case_id").drop_duplicates()
-    train_df, test_df = train_test_split(data.iloc[:,2:].dropna(), test_size=0.1, random_state=42)
+# for RNA-seq data
+clinical_df = pd.read_csv(CLINICAL)
+transcriptomic_df = pd.read_csv(TRANSCRIPTOMIC)
+diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
+data = transcriptomic_df.merge(diagnosis, on = "case_id").drop_duplicates()
+x = data.loc[:, data.columns != "vital_status_dead"]
+y = data["vital_status_Dead"].astype(int)
+x_ros, y_ros = ros.fit_resample(x, y)
+X_train, X_test, y_train, y_test = train_test_split(x_ros.iloc[:, 2:].dropna(), y_ros, test_size=0.1, random_state=42)
+ae_versus_vae('transcriptomic', X_train, X_test, y_train, y_test)
 
-elif 'EPIGENOMIC'  == MODALITY_TESTING:
-    # for Epigenomic data
-    clinical_df = pd.read_csv(CLINICAL)
-    epigenomic_df = pd.read_csv(EPIGENOMIC)
-    diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
-    data = epigenomic_df.merge(diagnosis, on = "case_id").drop_duplicates()
-    train_df, test_df = train_test_split(data.iloc[:,2:].dropna(), test_size=0.1, random_state=42)
+# for CNV data
+clinical_df = pd.read_csv(CLINICAL)
+cnv_df = pd.read_csv(CNV)
+diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
+data = cnv_df.merge(diagnosis, on = "case_id").drop_duplicates()
+x = data.loc[:, data.columns != "vital_status_dead"]
+y = data["vital_status_Dead"].astype(int)
+x_ros, y_ros = ros.fit_resample(x, y)
+X_train, X_test, y_train, y_test = train_test_split(x_ros.iloc[:, 1:].dropna(), y_ros, test_size=0.1, random_state=42)
+ae_versus_vae('cnv',  X_train, X_test, y_train, y_test)
 
-x_train = normalize(train_df.drop('vital_status_Dead', axis = 1))
-x_test = normalize(test_df.drop('vital_status_Dead', axis = 1))
-y_train = train_df['vital_status_Dead']
-y_test = test_df['vital_status_Dead']
+# for Epigenomic data
+clinical_df = pd.read_csv(CLINICAL)
+epigenomic_df = pd.read_csv(EPIGENOMIC)
+diagnosis = clinical_df[["vital_status_Dead", "case_id"]]
+data = epigenomic_df.merge(diagnosis, on = "case_id").drop_duplicates()
+x = data.loc[:, data.columns != "vital_status_dead"]
+y = data["vital_status_Dead"].astype(int)
+x_ros, y_ros = ros.fit_resample(x, y)
+X_train, X_test, y_train, y_test = train_test_split(x_ros.iloc[:, 2:].dropna(), y_ros, test_size=0.1, random_state=42)
+ae_versus_vae('epigenomic',  X_train, X_test, y_train, y_test)
+
+
 
 # TODO: add the plot directory as an output
-for LATENT_DIM in LATENT_DIMS:
-    ## vae
-    tf.keras.backend.clear_session()
-    vae = MAVI.VAE(input_dim = x_train.shape[1], 
-        hidden_dim=HIDDEN_DIM,
-        latent_dim=LATENT_DIM,   
-        activation = 'relu',
-        lr = LEARNING_RATE_UNSUPERVISED)
-    vae.train(x_train, n_epochs= EPOCHS_UNSUPERVISED)
-    vae.plot_latent_space(x_train, y_train, output=f'/users/anair27/data/anair27/singh-lab-TCGA-project/multiomic-model-tcga/__plots/RNA_SEQ_VAE_UMAP_{LATENT_DIM}dim.png')
-    x_train_vae_embed = vae.get_embedding(x_train)
-    x_test_vae_embed = vae.get_embedding(x_test)
-    # unimodal model
-    tf.keras.backend.clear_session()
-    model_on_vae, _ = um.create_model(x_train = x_train_vae_embed, y_train = y_train, n_epochs = EPOCHS_SUPERVISED, batch_size = None, lr = LEARNING_RATE_SUPERVISED, n_hidden = 1, dim_hidden = LATENT_DIM / 2)
-    um.plot_confusion(model_on_vae, x_test_vae_embed, y_test, filepath = f'/users/anair27/data/anair27/singh-lab-TCGA-project/multiomic-model-tcga/__plots/RNA_SEQ_CM_MODEL_VAE_{LATENT_DIM}dim.png')
-    ## ae
-    tf.keras.backend.clear_session()
-    ae = um.create_ae(x_train = x_train, latent_dim = LATENT_DIM, epochs= EPOCHS_UNSUPERVISED, lr = LEARNING_RATE_UNSUPERVISED)
-    x_train_ae_embed = um.encode_input(ae, x_train)
-    x_test_ae_embed = um.encode_input(ae, x_test)
-    tf.keras.backend.clear_session()
-    model_on_ae, _ = um.create_model(x_train = x_train_ae_embed, y_train = y_train, n_epochs = EPOCHS_SUPERVISED, batch_size = None, lr = LEARNING_RATE_SUPERVISED, n_hidden = 1, dim_hidden = LATENT_DIM / 2)
-    um.plot_confusion(model_on_ae, x_test_ae_embed, y_test, filepath = f'/users/anair27/data/anair27/singh-lab-TCGA-project/multiomic-model-tcga/__plots/RNA_SEQ_CM_MODEL_AE_{LATENT_DIM}dim.png')
+
+
     
 
     
